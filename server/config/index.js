62b3c4492a4ddf769f1781f9d24c793215a19a3c
somesh955 <somesh955@gmail.com>
2017-04-12 02:25:17 +0530

    test

--- server/config/index.js
+++ server/config/index.js
@@ -1,4 +1,7 @@
 module.exports = {
   init: require('./passport'),
-  middleware: require('./middleware')
+  middleware: require('./middleware'),
+  response : require('./response'),
+  connection : require('./database'),
+  config : require('./config')
 }
