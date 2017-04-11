e7fb75048810676a1498cd1994fca628edea162e
somesh955 <somesh955@gmail.com>
2017-04-12 02:25:32 +0530

    test

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,36 +1,40 @@
-var express = require('express');
-var router = express.Router();
-var Response = require('../config/response')();
-var DBConnection= require('../config/database')();
-var PersonCtrl = require('../controllers/person.controller')();
+(function(){
+  'use-strict';
+  var express = require('express');
+  var router = express.Router();
+  var Response = require('../config/response')();
+  var DBConnection= require('../config/database')();
+  var PersonCtrl = require('../controllers/person.controller')();
+  var jwt = require('jsonwebtoken');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  PersonCtrl.getPersonList(function(err, result){
-      if(err) res.status(302).jsonp(err);
-      res.status(200).jsonp(result); 
-  });                    
-});
+  var authRoutes = function(upload){
+    router.get('/users', function(req, res, next) {
+      console.log(req);
+      PersonCtrl.getPersonList(function(err, result){
+          if(err) res.status(302).jsonp(err);
+          res.status(200).jsonp(result); 
+      });                    
+    });
 
-router.post('/', function(req, res, next) {
-   PersonCtrl.setPerson(req.body,function(err, result){
-      if(err) res.status(302).jsonp(err);
-      res.status(200).jsonp(result); 
-  });  
-});
+    router.post('/authenticate', function(req, res, next) {
+      console.log(req.body.username);
+      var myToken = jwt.sign({'username':req.body.username},"SOMESH@955");
+      res.status(200).json({"token":myToken});     
+    });
 
-const passport = require('passport')
+    router.post('/uploadFiles', function (req, res) {
+      upload(req, res, function (err) {
+        if (err) {
+          return res.end(err.toString());
+        }
+    
+        res.end('File is uploaded');
+      });
+    });
 
-router.get('/profile', passport.authenticationMiddleware(), renderProfile)  
-
-
-function authenticationMiddleware () {  
-  return function (req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-    res.redirect('/')
+    return router
   }
-}
 
-module.exports = router;
+  module.exports = authRoutes;
+
+}());
