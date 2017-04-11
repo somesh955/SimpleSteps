9d7102ff1484caeb87e93bb5cfe0e026ec78798d
somesh955 <somesh955@gmail.com>
2017-04-12 02:26:39 +0530

    test

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+(function(){
+    'use-strict';
+    var constant = {        
+        "server" : {"PORT":3002},
+        "redisStore" : {"SECRET":"Somesh@955"}        
+    }
+
+module.exports = constant;
+}());
